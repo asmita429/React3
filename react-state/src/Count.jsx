804d3b7bd61633a059d44deb80e8ba7161ba7a10
Asmita Thapa <asmitathapa429@gmail.com>
2024-04-05 15:23:27 +0545

    my first counter using useState hook

--- react-state/src/Count.jsx
+++ react-state/src/Count.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 export default function Count() {
-  let count = 0;
-  function incCount() {
-    count += 1;
-    console.log(count);
-  }
+  //   let count = 0;
+  //   function incCount() {
+  //     count += 1;
+  //     console.log(count);
+  //   }
+
+  let [count, setCount] = useState(0);
+
+  let incCount = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <>
+    <div>
       <h3>Count = {count}</h3>
       <button onClick={incCount}>Increase</button>
-    </>
+    </div>
   );
 }
