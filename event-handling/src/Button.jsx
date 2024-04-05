3463ea691998de65dca380cc56e749ffd24fcf4f
Asmita Thapa <asmitathapa429@gmail.com>
2024-04-05 13:18:01 +0545

    handling non click events example

--- event-handling/src/Button.jsx
+++ event-handling/src/Button.jsx
@@ -1,14 +1,23 @@
-function printHello() {
+function handleClick() {
   console.log("Hello ");
 }
-function printBye() {
-  console.log("bye");
+function handleMouseOver() {
+  console.log("Mouse moved over a paragraph ");
+}
+function handleDblClick() {
+  console.log("double clicked");
 }
 export default function Button() {
   return (
     <>
-      <button onClick={printHello}>Click me!</button>
-      <p onClick={printBye}>this paragraph says bye</p>
+      <button onClick={handleClick}>Click me!</button>
+      <p onMouseOver={handleMouseOver}>
+        this paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Sed excepturi ipsam laudantium at sunt suscipit similique illum a
+        recusandae in.
+      </p>
+
+      <button onDoubleClick={handleDblClick}>Double click!</button>
     </>
   );
 }
